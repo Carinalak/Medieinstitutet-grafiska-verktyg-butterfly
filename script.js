2b4cf60e65ea234973b557da2365075a69b0629d
Carinalak <carina.lakosil@gmail.com>
2024-04-21 12:01:18 +0200

    :construction: movement on eyebrows

--- script.js
+++ script.js
@@ -39,6 +39,54 @@ gsap.to('#utrop', {
 
 
 var svgElement = document.querySelector('#butterfly');
+//var eyebrowLeft = document.querySelector('#eyebrowLeft');
+
+svgElement.addEventListener('mouseenter', function(event) {
+    var rect = svgElement.getBoundingClientRect();
+    var mouseX = event.clientX - rect.left;
+    var mouseY = event.clientY - rect.top; 
+
+    var offsetX = mouseX > rect.width / 2 ? 10 : -10;
+    var offsetY = mouseY > rect.height / 2 ? 3 : -3;
+
+    gsap.to(['#eyeBlackRight', '#eyeBlackLeft'], {
+        x: offsetX,
+        y: offsetY,
+        duration: 0.5,
+    });
+
+    gsap.to(eyebrowLeft, {
+        rotation: -20,
+        duration: 0.5,
+    });
+    gsap.to(eyebrowRight, {
+        rotation: -10,
+        y: -5,
+        duration: 0.5,
+    });
+});
+
+svgElement.addEventListener('mouseleave', function() {
+    gsap.to(['#eyeBlackRight', '#eyeBlackLeft'], {
+        x: 0,
+        y: 0,
+        duration: 0.5,
+    });
+
+    gsap.to(eyebrowLeft, {
+        rotation: 0,
+        duration: 0.5,
+    });    
+        gsap.to(eyebrowRight, {
+            rotation: 0,
+            y: 0,
+            duration: 0.5,
+    });
+    
+});
+/*
+
+var svgElement = document.querySelector('#butterfly');
 
 svgElement.addEventListener('mouseenter', function(event) {
     var rect = svgElement.getBoundingClientRect();
@@ -62,4 +110,4 @@ svgElement.addEventListener('mouseleave', function() {
         duration: 0.5,
     });
 });
-
+*/
